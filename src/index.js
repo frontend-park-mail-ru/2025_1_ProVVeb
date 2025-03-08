dd6f182f5d253b364d3e38726623d8bae70f6d2c
Dan Yudin <dan0808ila@gmail.com>
2025-03-08 20:32:00 +0300

    fixed login

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import Notification from "./components/simple/errorMessage/errorMessage.js"
 
 // если не авторизован решать куда идти
 
-router.navigateTo('auth');
+router.navigateTo('login');
 
-const root = document.getElementById('root');
-const el0 = new HeaderGreeting(root);
-const error = new Notification(root, {isWarning: false, isWithButton: true, title: "Такой логин и пароль не зарегистрированы",})
-const el = new LoginInput(root);
-const el1 = new PasswordInput(root);
+// const root = document.getElementById('root');
+// const el0 = new HeaderGreeting(root);
+// const error = new Notification(root, {isWarning: false, isWithButton: true, title: "Такой логин и пароль не зарегистрированы",})
+// const el = new LoginInput(root);
+// const el1 = new PasswordInput(root);
 
-error.render();
-el0.render();
-el.render();
-el1.render();
+// error.render();
+// el0.render();
+// el.render();
+// el1.render();
 
--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -1,11 +1,13 @@
 import BaseComponent from "../../components/BaseComponent.js";
 import router from "../../modules/router.js";
 import LoginCard from "../../components/compound/loginCard/loginCard.js";
+import HeaderGreeting from "../../components/compound/headerGreeting/headerGreeting.js";
 
 export default class LoginPage extends BaseComponent {
 	constructor(parentElement) {
+		const headerGreeting = new HeaderGreeting(parentElement).template;
 		const loginCard = new LoginCard(parentElement).template;
-		super(loginCard, parentElement);
+		super(headerGreeting + loginCard, parentElement);
 
 		// this.addListener(
 		// 	'click',
