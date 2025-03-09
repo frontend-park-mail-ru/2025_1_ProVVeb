75a5d0871149f72193515f3fe899c370c5d3ea4c
Dan Yudin <dan0808ila@gmail.com>
2025-03-09 23:13:29 +0300

    fixed notifications

--- src/components/simple/notification/notification.js
+++ src/components/simple/notification/notification.js
@@ -6,7 +6,17 @@ export default class Notification extends BaseComponent {
         const templateHBS = Handlebars.templates['notification.hbs'];
         const templateHTML = templateHBS(paramsHBS);
         super(templateHTML, store.getState('notif_layer'));
+
+        this.addListener(
+            'click',
+            `.cross`,
+            (event) => {
+                event.target.parentElement.parentElement.remove();
+            }
+        );
     }
+
+    
 
     render() {
         this.parentElement.innerHTML = this.template;
