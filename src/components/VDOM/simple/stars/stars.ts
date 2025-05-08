976a679f92c2c9231fd3aa37f351d9d4fc66c50d
Shahmar <abdull.shah@mail.ru>
2025-05-08 21:11:17 +0300

    Small

--- src/components/VDOM/simple/stars/stars.ts
+++ src/components/VDOM/simple/stars/stars.ts
@@ -2,22 +2,22 @@ import { VBC } from '@modules/VDOM/VBC';
 import templateHBS from './stars.hbs';
 
 function convertPoints(point: number): boolean[] {
-	const ans: boolean[] = [];
-	for (let i = 1; i <= 5; i++) {
-		if (i <= point) { ans.push(true); } else { ans.push(false); }
-	}
+    const ans: boolean[] = [];
+    for (let i = 1; i <= 5; i++) {
+        if (i <= point) { ans.push(true); } else { ans.push(false); }
+    }
 
-	return ans;
+    return ans;
 }
 
 export class VStars extends VBC {
-	constructor(point: number) {
-		super(
-			templateHBS,
-			{ links: [false, false, false, false, false] },
-			'',
-			[],
-			{ links: convertPoints(point) }
-		);
-	}
+    constructor(point: number) {
+        super(
+            templateHBS,
+            { links: [false, false, false, false, false] },
+            '',
+            [],
+            { links: convertPoints(point) }
+        );
+    }
 }
